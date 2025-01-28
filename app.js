158e99044e06bb6ac22070abde951330caf150eb
tigejw <tigejw@gmail.com>
2025-01-28 16:31:34 +0000

    add functionality, error handling and testing for patch articles/article_id. also refactor previous handling of invalid username input errors to be more flexible and robus. create util checkusername + test to achieve this

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 
-const {getEndpoints, getTopics, getArticleByArticleId, getArticles, getCommentsByArticleId, postCommentByArticleId} = require("./controller")
+const { getEndpoints, getTopics, getArticleByArticleId, getArticles, getCommentsByArticleId, postCommentByArticleId, patchArticleByArticleId } = require("./controller")
 
 app.get("/api", getEndpoints)
 app.get("/api/topics", getTopics)
@@ -10,28 +10,36 @@ app.get("/api/articles/:article_id", getArticleByArticleId)
 app.get("/api/articles", getArticles)
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId)
 app.post("/api/articles/:article_id/comments", postCommentByArticleId)
+app.patch("/api/articles/:article_id", patchArticleByArticleId)
 
-app.all("/*", (req, res)=>{
-    res.status(404).send({error: "Invalid URL!"})
+
+app.all("/*", (req, res) => {
+    res.status(404).send({ error: "Invalid URL!" })
 })
 
 //error handling middleware
 
-app.use((err, req, res, next)=>{
-    if(err.code==="22P02" || err.code==="23502" || err.code==="23503"){
-        res.status(400).send({error: "Bad request!"})
-    }else next(err)
+app.use((err, req, res, next) => {
+    if (err.code === "22P02" || err.code === "23502") {
+        res.status(400).send({ error: "Bad request!" })
+    } else next(err)
 })
 
-app.use((err, req, res, next)=>{
-    if(err.status && err.msg){
-        res.status(err.status).send({error: err.msg})
-    }else next(err)
+app.use((err, req, res, next) => {
+    if (err.status && err.msg) {
+        res.status(err.status).send({ error: err.msg })
+    } else next(err)
+})
+
+app.use((err, req, res, next) => {
+    if (err.code === "23503") {
+        res.status(404).send({ error: "Not found!" })
+    } else next(err)
 })
 
 app.use((err, req, res, next) => {
     console.log(err, "<<< handle this")
-    res.status(500).send({ error: "Server Error!"});
-  });
+    res.status(500).send({ error: "Server Error!" });
+});
 
 module.exports = app
