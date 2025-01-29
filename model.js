822ae9ada933e04ead83851c1b57fef45b16bc05
tigejw <tigejw@gmail.com>
2025-01-29 14:40:54 +0000

    add comment_count property to get /api/articles/:article_id

--- model.js
+++ model.js
@@ -18,7 +18,8 @@ exports.selectAllTopics = () => {
 exports.selectArticleByArticleId = (article_id) => {
     return checkExists("articles", "article_id", article_id).
     then(() => {
-        return db.query("SELECT * FROM articles WHERE article_id = $1", [article_id]).then(({ rows }) => {
+        return db.query("SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.body, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id)::int AS comment_count FROM articles JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id", [article_id])
+        .then(({ rows }) => {
             if (rows.length === 0) {
                 return Promise.reject({ status: 404, msg: "Not found!" })
             }
