185ef36602eba3b5e125bf616d9a9971c4b73916
tigejw <tigejw@gmail.com>
2025-01-30 10:45:12 +0000

    troubleshoots error in hosting setup

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,11 @@
-const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require('pg');
+
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
 const config = {}
-
 if(ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
@@ -13,10 +13,6 @@ if(ENV === "production") {
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
-}
-
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
 }
 
 module.exports = new Pool(config);
