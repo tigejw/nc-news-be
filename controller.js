03f814e9eda1ebe0de6306bb090820dffedd7c78
tigejw <tigejw@gmail.com>
2025-01-31 15:27:49 +0000

    add functionality for post /api/articles

--- controller.js
+++ controller.js
@@ -1,4 +1,4 @@
-const { readEndpointsData, selectAllTopics, selectArticleByArticleId, selectAllArticles, selectCommentsByArticleId, insertCommentByArticleId, updateArticleByArticleId, deleteFromCommentsByCommentId, selectAllUsers, selectUserByUsername, updateCommentByCommentId } = require("./model.js")
+const { readEndpointsData, selectAllTopics, selectArticleByArticleId, selectAllArticles, selectCommentsByArticleId, insertCommentByArticleId, updateArticleByArticleId, deleteFromCommentsByCommentId, selectAllUsers, selectUserByUsername, updateCommentByCommentId, insertArticle } = require("./model.js")
 
 exports.getEndpoints = (req, res, next) => {
     readEndpointsData().then((endpointsData) => {
@@ -60,7 +60,6 @@ exports.postCommentByArticleId = (req, res, next) => {
         .catch((err) => {
             next(err)
         })
-
 }
 
 exports.patchArticleByArticleId = (req, res, next) => {
@@ -84,40 +83,48 @@ exports.deleteCommentByCommentId = (req, res, next) => {
         .catch((err) => {
             next(err)
         })
-
-
 }
 
 exports.getUsers = (req, res, next) => {
     selectAllUsers()
-    .then((users)=>{
-        res.status(200).send({users : users})
-    })
-    .catch((err)=>{
-        next(err)
-    })
+        .then((users) => {
+            res.status(200).send({ users: users })
+        })
+        .catch((err) => {
+            next(err)
+        })
 }
 
 exports.getUserByUsername = (req, res, next) => {
-    const {username} = req.params
+    const { username } = req.params
     selectUserByUsername(username)
-    .then((user)=>{
-        res.status(200).send({user : user})
-    })
-    .catch((err)=>{
-        next(err)
-    })
+        .then((user) => {
+            res.status(200).send({ user: user })
+        })
+        .catch((err) => {
+            next(err)
+        })
 }
 
 exports.patchCommentByCommentId = (req, res, next) => {
-    const {comment_id} = req.params
+    const { comment_id } = req.params
     const { inc_votes } = req.body
     updateCommentByCommentId(inc_votes, comment_id)
-    .then((comment)=>{
-        res.status(200).send({comment: comment})
+        .then((comment) => {
+            res.status(200).send({ comment: comment })
+        })
+        .catch((err) => {
+            next(err)
+        })
+}
+
+exports.postArticle = (req, res, next) => {
+    const { author, title, body, topic, article_img_url } = req.body
+    insertArticle(author, title, body, topic, article_img_url)
+    .then((article)=>{
+        res.status(201).send({article: article})
     })
     .catch((err)=>{
         next(err)
     })
-
 }
--- model.js
+++ model.js
@@ -17,15 +17,15 @@ exports.selectAllTopics = () => {
 
 exports.selectArticleByArticleId = (article_id) => {
     return checkExists("articles", "article_id", article_id).
-    then(() => {
-        return db.query("SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.body, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id)::int AS comment_count FROM articles JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id", [article_id])
-        .then(({ rows }) => {
-            if (rows.length === 0) {
-                return Promise.reject({ status: 404, msg: "Not found!" })
-            }
-            return rows[0]
+        then(() => {
+            return db.query("SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.body, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id)::int AS comment_count FROM articles JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id", [article_id])
+                .then(({ rows }) => {
+                    if (rows.length === 0) {
+                        return Promise.reject({ status: 404, msg: "Not found!" })
+                    }
+                    return rows[0]
+                })
         })
-    })
 }
 
 exports.selectAllArticles = (sort_by = 'created_at', order = 'desc', topic) => {
@@ -33,40 +33,40 @@ exports.selectAllArticles = (sort_by = 'created_at', order = 'desc', topic) => {
     let dbQuery = `SELECT title, articles.author, articles.article_id, topic, articles.created_at, articles.votes, article_img_url, COUNT(comments.comment_id)::int AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id `
     const queryValues = []
 
-    if(!greenList.includes(sort_by) || !greenList.includes(order)){
-        return Promise.reject({status: 400 , msg: "Invalid query!"})
+    if (!greenList.includes(sort_by) || !greenList.includes(order)) {
+        return Promise.reject({ status: 400, msg: "Invalid query!" })
     }
 
-    if(topic){
+    if (topic) {
         return checkExists("topics", "slug", topic)
-        .then(()=>{
-            queryValues.push(topic)
-            dbQuery += `WHERE articles.topic = $1 GROUP BY articles.article_id ORDER BY ${sort_by} ${order};`   
-            return db.query(dbQuery, queryValues)
-        })
-        .then(({ rows }) => {
-            return rows
-        })
-    }else{
+            .then(() => {
+                queryValues.push(topic)
+                dbQuery += `WHERE articles.topic = $1 GROUP BY articles.article_id ORDER BY ${sort_by} ${order};`
+                return db.query(dbQuery, queryValues)
+            })
+            .then(({ rows }) => {
+                return rows
+            })
+    } else {
         dbQuery += `GROUP BY articles.article_id ORDER BY ${sort_by} ${order};`
         return db.query(dbQuery)
-        .then(({ rows }) => {
-            return rows
-        })
+            .then(({ rows }) => {
+                return rows
+            })
     }
-   
-    
-    
+
+
+
 }
 
 exports.selectCommentsByArticleId = (article_id) => {
     return checkExists("articles", "article_id", article_id)
-    .then(() => {
-        return db.query(`SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC;`, [article_id])
-            .then(({ rows }) => {
-                return rows
-            })
-    })
+        .then(() => {
+            return db.query(`SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC;`, [article_id])
+                .then(({ rows }) => {
+                    return rows
+                })
+        })
 }
 
 exports.insertCommentByArticleId = (article_id, body) => {
@@ -93,7 +93,7 @@ exports.updateArticleByArticleId = (article_id, body) => {
                 })
         })
     }
-    
+
 }
 
 exports.deleteFromCommentsByCommentId = (comment_id) => {
@@ -104,30 +104,44 @@ exports.deleteFromCommentsByCommentId = (comment_id) => {
 
 exports.selectAllUsers = () => {
     return db.query("SELECT username, name, avatar_url FROM users")
-    .then(({rows})=>{
-        return rows
-    })
+        .then(({ rows }) => {
+            return rows
+        })
 }
 
 exports.selectUserByUsername = (username) => {
     return checkExists("users", "username", username)
-    .then(()=>{
-        return db.query("SELECT * FROM users WHERE username = $1", [username])
-    })
-    .then(({rows})=>{
-        return rows[0]
-    })
+        .then(() => {
+            return db.query("SELECT * FROM users WHERE username = $1", [username])
+        })
+        .then(({ rows }) => {
+            return rows[0]
+        })
 }
 
 exports.updateCommentByCommentId = (inc_votes, comment_id) => {
-    if(typeof inc_votes !== "number"){
-        return Promise.reject({status: 400, msg: "Bad request!"})
+    if (typeof inc_votes !== "number") {
+        return Promise.reject({ status: 400, msg: "Bad request!" })
     }
     return checkExists("comments", "comment_id", comment_id)
-    .then(()=>{
-        return db.query("UPDATE comments SET votes = votes + $1 WHERE comment_id = $2 RETURNING *;", [inc_votes, comment_id] )
-    })
-    .then(({rows})=>{
-        return rows[0]
-    })
+        .then(() => {
+            return db.query("UPDATE comments SET votes = votes + $1 WHERE comment_id = $2 RETURNING *;", [inc_votes, comment_id])
+        })
+        .then(({ rows }) => {
+            return rows[0]
+        })
+}
+
+exports.insertArticle = (author, title, body, topic, article_img_url = "https://images.pexels.com/photos/97050/pexels-photo-97050.jpeg?w=700&h=70") => {
+    return checkExists("users", "username", author)
+        .then(()=>{
+            return checkExists("topics", "slug", topic)
+        })
+        .then(() => {
+            return db.query("INSERT INTO articles (author, title, body, topic, article_img_url) VALUES ($1, $2, $3, $4, $5) RETURNING *", [author, title, body, topic, article_img_url])
+        })
+        .then(({ rows }) => {
+            rows[0].comment_count = 0
+            return rows[0]
+        })
 }
