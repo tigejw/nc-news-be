adf0a96f93ddf83387eda3767e3f1712daf2de0c
tigejw <184505770+tigejw@users.noreply.github.com>
2025-02-17 10:22:15 +0000

    install and setup cors in app.js

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
-const express = require("express")
-const app = express()
-app.use(express.json())
-const apiRouter = require("./apiRouter")
-
+const express = require("express");
+const app = express();
+app.use(express.json());
+const apiRouter = require("./apiRouter");
+const cors = require("cors");
+app.use(cors());
 //endpoint routing
 
-app.use("/api", apiRouter)
+app.use("/api", apiRouter);
 
 //invalid url handling
 
 app.all("/*", (req, res) => {
-    res.status(404).send({ error: "Invalid URL!" })
-})
+  res.status(404).send({ error: "Invalid URL!" });
+});
 
 //error handling middleware
 
 app.use((err, req, res, next) => {
-    if (err.code === "22P02" || err.code === "23502") {
-        res.status(400).send({ error: "Bad request!" })
-    } else next(err)
-})
-
-app.use((err, req, res, next) => {
-    if (err.status && err.msg) {
-        res.status(err.status).send({ error: err.msg })
-    } else next(err)
-})
-
-app.use((err, req, res, next) => {
-    console.log(err, "<<< handle this")
-    res.status(500).send({ error: "Server Error!", msg: err });
+  if (err.code === "22P02" || err.code === "23502") {
+    res.status(400).send({ error: "Bad request!" });
+  } else next(err);
 });
 
-module.exports = app
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ error: err.msg });
+  } else next(err);
+});
+
+app.use((err, req, res, next) => {
+  console.log(err, "<<< handle this");
+  res.status(500).send({ error: "Server Error!", msg: err });
+});
+
+module.exports = app;
